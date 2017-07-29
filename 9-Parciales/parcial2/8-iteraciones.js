a825fb7a84ed333f9d01f8b1bc4017dcb392a4fc
FigueirasJuan <tec.jmfigueiras@gmail.com>
2017-07-29 11:27:25 -0300

    Parcial 2 terminado.

--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,60 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var num;
+	var respuesta = true;
+	var suma = 0;
+	var contador = 0;
+	var contpar = 0;
+	var promedio;
+	var max;
+	var min;
+
+while(respuesta == true)
+{
+	num = prompt("Ingrese un número positivo");
+	num = parseInt(num);
+
+	while(num<0)
+	{
+		num = prompt("Ingrese un número positivo!");
+	}
+
+	if((num%2)==0)
+	{
+		contpar++;
+	}
+	
+	if(contador==1)
+	{
+		max = num;
+		min = num;
+	}
+	else if(num>min)
+	{
+		max = num;
+	}
+	else
+	{
+		min = num;
+	}
+
+
+	suma = suma + num;
+	contador++;
+
+	respuesta = confirm("Desea seguir ingresando números?");
+
+}
+
+promedio = (suma / contador);
+
+document.write("<h1> Cantidad de números pares </h1>" + contpar);
+document.write("<h1> Promedio </h1>" + promedio);
+document.write("<h1> Suma </h1>" + suma);
+document.write("<h1> Máximo </h1>" + max);
+document.write("<h1> Mínimo </h1>" + min);
+
 	
 }
 
